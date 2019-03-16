d9703f4f678ce900ab96c36106465ca60012bea6
Isaac Buitrago <mightyking7@gmail.com>
2019-03-16 16:23:43 -0500

    Imported routes and defined modules to use for routing.

--- src/server.js
+++ src/server.js
@@ -1,6 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
+// routes
+const users = require("../routes/api/users");
+const profile = require("../routes/api/profile");
+const posts = require("../routes/api/posts");
+
+// db connection string
+const db = require("../config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+// entry point for API
 const app = express();
+
+// define Routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 app.get("/", (req, res) => res.send("Hello World! Isaac"));
 
