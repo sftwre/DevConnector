f7ff3a844c697dc06213d3b3b271e0507f225a6c
Isaac Buitrago <mightyking7@gmail.com>
2019-06-14 00:39:35 -0500

    Imported validation, User, post, and profile models

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,11 @@
 const express = require("express");
 
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
+const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
 
 // @route  GET api/posts/test
 // @desc   Test posts route
