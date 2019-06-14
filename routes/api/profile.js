12094664754b2f24ee00a5ed864585b120de729c
Isaac Buitrago <mightyking7@gmail.com>
2019-06-13 21:37:18 -0500

    Removed profile from experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -183,7 +183,6 @@ router.delete("/", auth, async (req, res) => {
 // @route   DELETE api/profile/experience/:exp_id
 // @desc    Delete experience from profile
 // @access Private
-router.delete("/profile/experience/:exp_id", auth, async (req, res) => {
 router.delete("/experience/:exp_id", auth, async (req, res) => {
   try {
     const profile = await Profile.findOne({ user: req.user.id });
