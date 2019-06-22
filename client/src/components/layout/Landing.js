d75753eb3e8b530f8ca971e0efbf80ab16c0cc49
Isaac Buitrago <mightyking7@gmail.com>
2019-06-22 14:27:00 -0500

    Replaced anchor tags with links to work with react-router

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Landing extends Component {
   render() {
@@ -12,12 +13,12 @@ class Landing extends Component {
               from other developers
             </p>
             <div className="buttons">
-              <a href="register.html" className="btn btn-primary">
+              <Link to="register" className="btn btn-primary">
                 Sign Up
-              </a>
-              <a href="login.html" className="btn btn-light">
+              </Link>
+              <Link to="login" className="btn btn-light">
                 Login
-              </a>
+              </Link>
             </div>
           </div>
         </div>
