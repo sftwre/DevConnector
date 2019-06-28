a8bd731ca6e052c4fbe0a7d680eadb4e70311569
Isaac Buitrago <mightyking7@gmail.com>
2019-06-27 21:26:03 -0500

    Created function to load a user profile with a token stored in local storage.

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,4 +1,9 @@
-import { REGISTER_SUCCESS, REGISTER_FAIL } from "../actions/types";
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR
+} from "../actions/types";
 
 const initialState = {
   token: localStorage.getItem("token"),
@@ -16,6 +21,7 @@ export default function(state = initialState, action) {
 
       return { ...state, ...payload, isAuthenticated: true, loading: false };
 
+    case AUTH_ERROR:
     case REGISTER_FAIL:
       localStorage.removeItem("token");
       return {
@@ -23,6 +29,14 @@ export default function(state = initialState, action) {
         token: null,
         isAuthenticated: false,
         loading: false
+      };
+
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: payload
       };
 
     default:
