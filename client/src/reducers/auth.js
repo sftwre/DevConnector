d709ee3df22e260327837b392c76c978795379ac
Isaac Buitrago <mightyking7@gmail.com>
2019-06-29 22:40:30 -0500

    Added new action types for authentication.

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -2,7 +2,9 @@ import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
   USER_LOADED,
-  AUTH_ERROR
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL
 } from "../actions/types";
 
 const initialState = {
@@ -16,11 +18,13 @@ export default function(state = initialState, action) {
   const { type, payload } = action;
 
   switch (type) {
+    case LOGIN_SUCCESS:
     case REGISTER_SUCCESS:
       localStorage.setItem("token", payload.token);
 
       return { ...state, ...payload, isAuthenticated: true, loading: false };
 
+    case LOGIN_FAIL:
     case AUTH_ERROR:
     case REGISTER_FAIL:
       localStorage.removeItem("token");
