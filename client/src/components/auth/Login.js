3646bc028067bfc539bd0d3669569a67ae3d8f81
Isaac Buitrago <mightyking7@gmail.com>
2019-06-30 22:44:56 -0500

    Added isAuthenticated flag to props and used it to redirect authenticated users to the dashboard.

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,10 @@
 import React, { useState, Fragment } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { login } from "../../actions/auth";
+import PropTypes from "prop-types";
 
-const Login = () => {
+const Login = ({ login, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     email: "",
     password: ""
@@ -26,8 +29,13 @@ const Login = () => {
   const onSubmit = async e => {
     e.preventDefault();
 
-    console.log("SUCCESS");
+    login(email, password);
   };
+
+  // redirect if authenticated
+  if (isAuthenticated) {
+    return <Redirect to="/dashboard" />;
+  }
 
   return (
     <Fragment>
@@ -65,4 +73,16 @@ const Login = () => {
   );
 };
 
-export default Login;
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(
+  mapStateToProps,
+  { login }
+)(Login);
