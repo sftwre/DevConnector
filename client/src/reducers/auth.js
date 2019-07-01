56b79248e7fd45ee14fc0a5abce25ab5a336a18f
Isaac Buitrago <mightyking7@gmail.com>
2019-06-30 23:10:38 -0500

    Added logout feature that determines links available on the navbar.

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -4,7 +4,8 @@ import {
   USER_LOADED,
   AUTH_ERROR,
   LOGIN_SUCCESS,
-  LOGIN_FAIL
+  LOGIN_FAIL,
+  LOGOUT
 } from "../actions/types";
 
 const initialState = {
@@ -27,6 +28,7 @@ export default function(state = initialState, action) {
     case LOGIN_FAIL:
     case AUTH_ERROR:
     case REGISTER_FAIL:
+    case LOGOUT:
       localStorage.removeItem("token");
       return {
         ...state,
