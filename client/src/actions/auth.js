316ec34fe3fbe3d84e2a7b35a485a2892fd27642
Isaac Buitrago <mightyking7@gmail.com>
2019-06-30 22:42:55 -0500

    Created action to authenticate a user and dispatch a state change to the user.

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -4,7 +4,9 @@ import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
   USER_LOADED,
-  AUTH_ERROR
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL
 } from "./types";
 
 import setAuthToken from "../utils/setAuthToken";
@@ -16,6 +18,7 @@ export const loadUser = () => async dispatch => {
   }
 
   try {
+    // authenticate the user with the token
     const res = await axios.get("/api/auth");
 
     dispatch({
@@ -24,7 +27,7 @@ export const loadUser = () => async dispatch => {
     });
   } catch (err) {
     dispatch({
-      type: USER_LOADED
+      type: AUTH_ERROR
     });
   }
 };
@@ -52,6 +55,33 @@ export const register = ({ name, email, password }) => async dispatch => {
     }
     dispatch({
       type: REGISTER_FAIL
+    });
+  }
+};
+
+// Login user
+export const login = (email, password) => async dispatch => {
+  const config = {
+    headers: { "Content-Type": "application/json " }
+  };
+
+  const body = JSON.stringify({ email, password });
+
+  try {
+    const res = await axios.post("/api/auth", body, config);
+
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data
+    });
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, "danger")));
+    }
+    dispatch({
+      type: LOGIN_FAIL
     });
   }
 };
