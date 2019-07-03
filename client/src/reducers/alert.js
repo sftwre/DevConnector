60c19a78c409cf1887ecee340b7856d40840c0f8
Isaac Buitrago <mightyking7@gmail.com>
2019-07-02 23:13:59 -0500

    Used SET_ALERT action

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -5,7 +5,7 @@ export default function(state = initialState, action) {
   const { type, payload } = action;
 
   switch (type) {
-    case "SET_ALERT":
+    case SET_ALERT:
       return [...state, payload];
 
     case REMOVE_ALERT:
