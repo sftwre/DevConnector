8e687c21eabd6ba8c7d40a0da3fede7d9eb96a58
Isaac Buitrago <mightyking7@gmail.com>
2019-07-02 23:13:19 -0500

    Added profile reducer to root reducer. Created action for getting the current profile. Created reducer for actions related to the profile.

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
 import auth from "./auth";
 import alert from "./alert";
+import profile from "./profile";
 
-export default combineReducers({ alert, auth });
+export default combineReducers({ alert, auth, profile });
