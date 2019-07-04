0e0be172d10645f770305a0c2889cd5cf2fc572b
Isaac Buitrago <mightyking7@gmail.com>
2019-07-04 14:03:01 -0500

    Added fragment to notify user if they still have not created an account.

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import { getCurrentProfile } from "../../actions/profile";
+import { Link } from "react-router-dom";
 
 const Dashboard = ({
   getCurrentProfile,
@@ -24,7 +25,12 @@ const Dashboard = ({
       {profile !== null ? (
         <Fragment> has </Fragment>
       ) : (
-        <Fragment>has not</Fragment>
+        <Fragment>
+          <p>You have not yet setup a profile, please add some info</p>
+          <Link to="/create-profile" className="btn btn-primary my-1">
+            Create Profile
+          </Link>
+        </Fragment>
       )}
     </Fragment>
   );
