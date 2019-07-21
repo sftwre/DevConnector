d3bd7c423acce4036dc53df32833d3f2520f4051
Isaac Buitrago <mightyking7@gmail.com>
2019-07-21 18:19:19 -0500

    Passed true to createProfile function and refactored anchor into link

--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -80,7 +80,7 @@ const EditProfile = ({
   const onSubmit = e => {
     e.preventDefault();
 
-    createProfile(formData, history);
+    createProfile(formData, history, true);
   };
 
   return (
@@ -245,9 +245,9 @@ const EditProfile = ({
         )}
 
         <input type="submit" className="btn btn-primary my-1" />
-        <a className="btn btn-light my-1" href="dashboard.html">
+        <Link to="/dashboard" className="btn btn-light my-1">
           Go Back
-        </a>
+        </Link>
       </form>
     </Fragment>
   );
