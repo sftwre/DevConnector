ce6a2db4ac93d6497408eddb8f9c96f7025d4dbe
Isaac Buitrago <mightyking7@gmail.com>
2019-08-20 20:36:42 -0500

    made call to backend route for unliking a post synchronous

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -62,7 +62,7 @@ export const removeLike = id => async dispatch => {
 // Delete posts
 export const deletePost = id => async dispatch => {
   try {
-    axios.delete(`/api/posts/${id}`);
+    await axios.delete(`/api/posts/${id}`);
 
     dispatch({
       type: DELETE_POST,
