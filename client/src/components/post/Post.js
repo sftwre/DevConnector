4eb7c16dc77f613c7fec9f6a4c30db8a1d9a79b6
Isaac Buitrago <mightyking7@gmail.com>
2019-08-25 12:02:29 -0500

    Fetched data about a single post from backend and displayed in it's own component.

---
+++ client/src/components/post/Post.js
@@ -0,0 +1,42 @@
+import React, { Fragment, useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import Spinner from "../layout/Spinner";
+import { Link } from "react-router-dom";
+import PostItem from "../posts/PostItem";
+import CommentForm from "../post/CommentForm";
+import { getPost } from "../../actions/post";
+
+const Post = ({ getPost, post: { post, loading }, match }) => {
+  useEffect(() => {
+    getPost(match.params.id);
+  }, [getPost]);
+
+  return loading || post === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <Link to="/posts" className="btn">
+        Back To Posts
+      </Link>
+      <PostItem post={post} showActions={false} />
+      <CommentForm postId={post._id} />
+    </Fragment>
+  );
+};
+
+Post.propTypes = {
+  getPost: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => {
+  return {
+    post: state.post
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { getPost }
+)(Post);
