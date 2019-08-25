5b0c0ae4af8d92d298bf3669722029f75b338c09
Isaac Buitrago <mightyking7@gmail.com>
2019-08-25 12:03:47 -0500

    Added route to display a single post

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import EditProfile from "./components/profile-forms/EditProfile";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
 import Posts from "./components/posts/Posts";
+import Post from "./components/post/Post";
 import AddExpierence from "./components/profile-forms/AddExpierence";
 import AddEducation from "./components/profile-forms/AddEducation";
 import "./App.css";
@@ -68,6 +69,7 @@ const App = () => {
                 component={AddEducation}
               />
               <PrivateRoute exact path="/posts" component={Posts} />
+              <PrivateRoute exact path="/posts/:id" component={Post} />
             </Switch>
           </section>
         </Fragment>
