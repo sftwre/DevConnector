267fad819d941180fb4cebf906c26288d30837d3
Isaac Buitrago <mightyking7@gmail.com>
2019-08-25 20:38:33 -0500

    Prepare for deployment

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -153,9 +153,9 @@ export const addComment = (postId, formData) => async dispatch => {
 };
 
 // Delete comment
-export const removeComment = (postId, commentId) => async dispatch => {
+export const deleteComment = (postId, commentId) => async dispatch => {
   try {
-    const res = await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+    await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
 
     dispatch({
       type: REMOVE_COMMENT,
--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const users = require("../routes/api/users");
 const profile = require("../routes/api/profile");
 const posts = require("../routes/api/posts");
 const auth = require("../routes/api/auth");
+const path = require("path");
 
 // db connection string
 const connectDB = require("../config/db");
@@ -24,6 +25,13 @@ app.use("/api/auth", auth);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
-const port = process.env.port || 5000;
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+const port = process.env.port || 5000;
